564c0b19a14df6ca32575249e5e9a37575ce49f1
aldiariq <aldiariq@github.com>
2020-09-05 09:57:36 +0700

    selesai membuat splash screen

--- app/src/main/java/com/example/projektakripto/SplashScreenActivity.java
+++ app/src/main/java/com/example/projektakripto/SplashScreenActivity.java
@@ -1,13 +1,55 @@
 package com.example.projektakripto;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private ProgressBar pbloading;
+    private int Progress;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        initView();
+
+        new Thread(new Runnable() {
+            public void run() {
+                while (Progress < 100) {
+                    Progress += 1;
+                    handler.post(new Runnable() {
+                        public void run() {
+                            pbloading.setProgress(Progress);
+                        }
+                    });
+                    try {
+                        Thread.sleep(50);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                Intent pindahhalamanmasuk = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(pindahhalamanmasuk);
+            }
+        }).start();
+    }
+
+    private void initView(){
+        pbloading = (ProgressBar) findViewById(R.id.pbloadingHalamansplashscreen);
+        Progress = pbloading.getProgress();
+        handler = new Handler();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 }
