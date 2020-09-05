a10fa9fd7fec1264df61132a9a7ee119ad861071
aldiariq <aldiariq@github.com>
2020-09-05 10:10:53 +0700

    selesai modifikasi menu navigation drawer

--- app/src/main/java/com/example/projektakripto/DashboardActivity.java
+++ app/src/main/java/com/example/projektakripto/DashboardActivity.java
@@ -44,7 +44,7 @@ public class DashboardActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_keluar)
+                R.id.nav_halaman_utama, R.id.nav_keluar)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
